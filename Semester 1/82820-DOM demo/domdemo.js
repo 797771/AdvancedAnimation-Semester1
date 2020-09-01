17356bff1a636ed425aa00fe56166137c44f15b3
Isabelle Cobb <797771@seq.org>
2020-09-01 11:15:06 -0700

    9/1/20

--- Semester 1/82820-DOM demo/domdemo.js
+++ Semester 1/82820-DOM demo/domdemo.js
@@ -1,10 +1,15 @@
-window.onload = function() {
-  styleCanv();
-  addElement();
-};
+window.onload=examples;
 
-function styleCanv(){
-};
+function examples(){
 
-function addElement () {
- };
+// create a couple of elements in an otherwise empty HTML page
+var heading = document.createElement("h1");
+var heading_text = document.createTextNode("");
+
+// places heading in node tree as child of body
+document.body.appendChild(heading);
+
+// places heading_text in node tree as child of heading
+heading.appendChild(heading_text);
+
+}
