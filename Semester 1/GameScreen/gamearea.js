401ef8672a018f4e16ddaa3cbdce30f119568042
Isabelle Cobb <797771@seq.org>
2020-09-17 15:36:48 -0700

    9/17/20

--- Semester 1/GameScreen/gamearea.js
+++ Semester 1/GameScreen/gamearea.js
@@ -1,4 +1,5 @@
 var tiles=[];
+var buttons=[];
 
 function GameArea(){
   //Wrapper Div
@@ -12,6 +13,18 @@ function GameArea(){
   for(var i=0; i<4; i++){
     tiles[i]=this.tileDiv=document.createElement("div");
     this.tileMenuDiv.appendChild(tiles[i]);
+    tiles[i].setAttribute("style", "padding:20px;width:45px;height:35px;margin-top: 50px;background-color: white;border: solid black 2px;position:relative;left:805px; top: 115px;");
+  }
+
+  // create buttonMenuDiv
+  this.buttonMenuDiv = document.createElement("div");
+  this.wrapperDiv.appendChild(this.buttonMenuDiv);
+
+  //create 4 button divs
+  for(var i=0; i<4; i++){
+    buttons[i]=this.buttonDiv=document.createElement("div");
+    this.buttonMenuDiv.appendChild(buttons[i]);
+    buttons[i].setAttribute("style", "padding:20px;width:55px;height:45px;margin-right:100px;margin-top: 50px;border-radius:100%;background-color: white;border: solid black 2px;float:left;position:relative;bottom:510px;left:60px;");
   }
 
 }
