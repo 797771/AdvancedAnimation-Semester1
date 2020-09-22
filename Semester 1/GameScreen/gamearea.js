c67f688d7bf5d891d8168844fbdb93cb23c167a7
Isabelle Cobb <797771@seq.org>
2020-09-22 11:13:52 -0700

    CW-9/22/20

--- Semester 1/GameScreen/gamearea.js
+++ Semester 1/GameScreen/gamearea.js
@@ -9,29 +9,30 @@ function GameArea(){
   // create tileMenuDiv
   this.tileMenuDiv = document.createElement("div");
   this.wrapperDiv.appendChild(this.tileMenuDiv);
+  this.tileMenuDiv.id="tileMenuDiv";
 
   //create 4 tile divs
   for(var i=0; i<4; i++){
     tiles[i]=this.tileDiv=document.createElement("div");
     this.tileMenuDiv.appendChild(tiles[i]);
-    tiles[i].setAttribute("style", "padding:20px;width:45px;height:35px;margin-top: 50px;background-color: white;border: solid black 2px;position:relative;left:805px; top: 115px;");
+    tiles[i].setAttribute("style","padding:20px;width:45px;height:35px;margin-top: 50px;background-color: white;border: solid black 2px;position:relative;left:5px;bottom:25px;");
   }
 
   // create buttonMenuDiv
   this.buttonMenuDiv = document.createElement("div");
   this.wrapperDiv.appendChild(this.buttonMenuDiv);
-
+  this.buttonMenuDiv.id="buttonMenuDiv";
 
   //create 4 button divs
   for(var i=0; i<4; i++){
     buttons[i]=this.buttonDiv=document.createElement("div");
     this.buttonMenuDiv.appendChild(buttons[i]);
-    buttons[i].setAttribute("style", "padding:20px;width:55px;height:45px;margin-right:100px;margin-top: 50px;border-radius:100%;background-color: white;border: solid black 2px;float:left;position:relative;bottom:510px;left:60px;");
+    buttons[i].setAttribute("style", "padding:20px;width:55px;height:45px;margin-right:100px;margin-top: 50px;border-radius:100%;background-color: white;border: solid black 2px;float:left;position:relative;bottom:45px;left: 50px;");
   }
 
   // create html canvas inside wrapperDiv
   canvas2=document.createElement("canvas");
   this.wrapperDiv.appendChild(canvas2);
-  canvas2.setAttribute("style", "background-color: purple;height: 500px;width: 800px;position: relative;bottom:505px;");
+  canvas2.setAttribute("style", "background-color: black;height: 500px;width: 800px;position: relative;bottom:505px;");
 
 }
