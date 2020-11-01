b0f7742233fc61e54f1c23d40104b02e81e996f3
Isabelle Cobb <797771@seq.org>
2020-11-01 10:08:57 -0800

    11/1/20

--- Semester 1/Lab1030-Snakes/gamearea.js
+++ Semester 1/Lab1030-Snakes/gamearea.js
@@ -3,6 +3,6 @@ function GameArea(){
     let canvas =  document.getElementById("canvas"); // gets its style from mystyles.css
     // canvas.width = window.innerWidth;
     // canvas.height = window.innerHeight;
-    canvas.width = 700;
-    canvas.height = 500;
+    canvas.width = 1250;
+    canvas.height = 550;
 }
