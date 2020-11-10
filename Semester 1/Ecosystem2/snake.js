576b58bde33a2fae3d37f77d1a663b6fed9567b1
Isabelle Cobb <797771@seq.org>
2020-11-09 19:21:32 -0800

    Snakes added to Ecosystem

--- Semester 1/Ecosystem2/snake.js
+++ Semester 1/Ecosystem2/snake.js
@@ -22,26 +22,28 @@ Snake.prototype.run = function(){
 Snake.prototype.render = function(){
     let ctx = game.ctx;
     for(var i = 0;i<this.numSegments;i++){
-      ctx.strokeStyle = this.clr;
-      ctx.beginPath();
-      ctx.arc(this.segments[i].x, this.segments[i].y, 5, Math.PI*2, 0, false);
-      ctx.lineWidth= 2;
-      ctx.stroke();
-      //ctx.fill();
-      // ctx.stroke();
-      // ctx.fill();
-      // ctx.save();
-      // ctx.beginPath();
-      // ctx.translate(this.segments[i].x, this.segments[i].y);
-      // ctx.moveTo(-5, 0);
-      // ctx.lineTo(0, 10);
-      // ctx.lineTo(5, 0);
-      // ctx.lineTo(0, -10);
-      // ctx.lineTo(-5, 0);
-      // ctx.lineWidth=4;
-      // ctx.stroke();
-      // ctx.fill();
-      // ctx.restore();
+      // if(i==0){
+      //   ctx.strokeStyle = this.clr;
+      //   ctx.save();
+      //   ctx.beginPath();
+      //   ctx.translate(this.segments[i].x, this.segments[i].y);
+      //    ctx.moveTo(-8, -8);
+      //   // ctx.lineTo(0, 10);
+      //   // ctx.lineTo(6, -8);
+      //   ctx.lineTo(-10, 1);
+      //   ctx.lineTo(10, 1);
+      //   ctx.lineTo(-8, -8);
+      //   ctx.lineWidth = 2;
+      //   ctx.stroke();
+      //   ctx.restore();
+      // }
+      // else{
+        ctx.strokeStyle = this.clr;
+        ctx.beginPath();
+        ctx.arc(this.segments[i].x, this.segments[i].y, 5, Math.PI*2, 0, false);
+        ctx.lineWidth= 2;
+        ctx.stroke();
+      //}
     }
 
   }
