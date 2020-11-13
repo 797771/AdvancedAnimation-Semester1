210a008b3bb51ff2ad0ff3df69237174c8bfad6d
Isabelle Cobb <797771@seq.org>
2020-11-13 11:11:06 -0800

    particle systems partially working

---
+++ Semester 1/1113-Particle Systems/particle.js
@@ -0,0 +1,38 @@
+function Particle(x, y, rad, clr){
+  this.location = new JSVector(x, y);//initialize location at emitter location
+  this.velocity = new JSVector(Math.random()*10-1, Math.random()*2-2);
+  this.acceleration = new JSVector(0, -0.05);
+  this.lifespan = 225;
+  this.radius = rad;
+  this.clr = clr;
+}
+
+Particle.prototype.run = function(){
+  this.update();
+  this.render();
+}
+
+Particle.prototype.render = function(){
+  let ctx = game.ctx;
+  ctx.strokeStyle = this.clr;
+  ctx.fillStyle = this.clr;
+  ctx.beginPath();
+  ctx.arc(this.location.x,this.location.y, this.radius, Math.PI*2, 0, false);
+  ctx.stroke();
+  ctx.fill();
+}
+
+Particle.prototype.update = function(){
+  this.velocity.add(this.acceleration);
+  this.location.add(this.velocity);
+  this.lifespan-=2;
+}
+
+Particle.prototype.isDead = function(){
+  if(this.lifespan<0){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
