7b70882943e8085d7ae3615a416a9a78cbe05766
Isabelle Cobb <797771@seq.org>
2020-11-16 10:12:04 -0800

    Particle system finished

--- Semester 1/1113-Particle Systems/game.js
+++ Semester 1/1113-Particle Systems/game.js
@@ -6,14 +6,16 @@ function Game(){
     this.canvas = document.getElementById('canvas');
     // get the context
     this.ctx = this.canvas.getContext('2d'); // This is the context
+
+    //create particle system
+    let x = this.canvas.width/2;
+    let y = this.canvas.height/2;
+    this.psystem = new ParticleSystem(x, y);
 }
 
 // function to run the game each animation cycle
 Game.prototype.run = function(){
   if(!this.gamePaused){
-    let x = this.canvas.width/2;
-    let y = this.canvas.height/2;
-    let psystem = new ParticleSystem(x, y);
-    psystem.run();
+    this.psystem.run();
   }
 }
