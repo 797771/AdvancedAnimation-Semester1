73c187eef45d2b0017005f93d01fb20011b4d7c6
Isabelle Cobb <797771@seq.org>
2021-01-05 11:13:29 -0800

    started lab 105 in class

---
+++ Semester 2/Lab 105-World/game.js
@@ -0,0 +1,101 @@
+function Game(){
+
+    this.canvas1 = document.getElementById('cnv1');
+    this.context1 = this.canvas1.getContext('2d');
+    this.canvas2 = document.getElementById('cnv2');
+    this.context2 = this.canvas2.getContext('2d');
+
+    this.canvas1Loc = new JSVector();
+
+    this.world = {
+        top: -1500,
+        left: -2000,
+        bottom: 1500,
+        right: 2000,
+        width: 4000,
+        height: 3000
+    }
+    // canvas2 is scaled according to the ratio of its
+    // height and width to the height and width of the world
+    // so that the entire world fits within canvas2
+    this.scaleX = this.canvas2.width/this.world.width;
+    this.scaleY = this.canvas2.height/this.world.height;
+
+    // add an event handler such that the a, s, w, d keys
+    // will reposition the canvas within the world.
+    window.addEventListener("keypress", function(event){
+        switch(event.code){
+            case "KeyW":
+                if(game.canvas1Loc.y+100 > game.world.top)
+                    game.canvas1Loc.y -= 20;
+                break;
+            case "KeyS":
+                if(game.canvas1Loc.y + game.canvas1.height -100 < game.world.bottom)
+                    game.canvas1Loc.y += 20;
+                break;
+            case "KeyA":
+                if(game.canvas1Loc.x+100 > game.world.left)
+                    game.canvas1Loc.x -= 20;
+                break;
+            case "KeyD":
+                if(game.canvas1Loc.x + game.canvas1.width -100 < game.world.right)
+                    game.canvas1Loc.x += 20;
+                break;
+            break;
+            }
+    }, false);
+
+}//++++++++++++++++++++++  end game constructor
+
+
+// function to run the game each animation cycle
+Game.prototype.run = function(){
+    let ctx1 = this.context1;
+    let cnv1 = this.canvas1;
+    let ctx2 = this.context2;
+    let cnv2 = this.canvas2;
+    ctx1.fillStyle =  "#505050";
+    ctx1.fillRect(0,0,cnv1.width,cnv1.height);
+    ctx2.fillStyle =  "#505050";
+    ctx2.fillRect(0,0,cnv2.width,cnv2.height);
+
+    // translate canvas1 according to the location of the canvas in the world
+    ctx1.save();
+    ctx1.translate(this.canvas1Loc.x*(-1), this.canvas1Loc.y*(-1));
+    ctx1.restore();
+    // draw the bounds of the world in canvas1
+    ctx1.save();
+    ctx1.strokeStyle = "rgba(0, 230, 64, 1)"
+    ctx1.beginPath();
+    ctx1.strokeRect(-2000, -1500, 4000, 3000);
+    ctx1.restore();
+    // draw the x and y axes of the world in canvas1
+    ctx1.save();
+    ctx1.strokeStyle = "rgba(240, 52, 52, 1)"
+    ctx1.beginPath();
+    ctx1.moveTo(0, -1500);
+    ctx1.lineTo(0, 1500);
+    ctx1.stroke();
+    ctx1.moveTo(-2000, 0);
+    ctx1.lineTo(2000, 0);
+    ctx1.stroke();
+    ctx1.restore();
+    // scale canvas2 to contain the entire world
+    ctx2.save();
+    ctx2.strokeStyle = "rgba(0, 230, 64, 1)"
+    ctx2.beginPath();
+    ctx1.strokeRect(-2000, -1500, 4000, 3000);
+    ctx2.scale(0.5, 0.5);
+    ctx2.strokeRect(-2000, -1500, 4000, 3000);
+    ctx2.restore();
+    // center the world in canvas2
+
+    // draw the x and y axes of the world
+
+
+    // draw the outline of canvas1 in canvas2
+
+    // run all the actors
+
+
+}
