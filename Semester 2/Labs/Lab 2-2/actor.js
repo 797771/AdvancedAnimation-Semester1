5687e7613d4b53b519640078b6a60dfc02f04384
Isabelle Cobb <797771@seq.org>
2021-02-02 11:08:27 -0800

    in class 2/2/21

---
+++ Semester 2/Labs/Lab 2-2/actor.js
@@ -0,0 +1,25 @@
+class Actor{
+constructor(){
+  this.location = new JSVector(0+es.cellWidth/2,0+es.cellHeight/2);
+}
+
+run(){
+  this.render();
+  this.update();
+}
+
+render(){
+
+}
+
+update(){
+
+}
+
+
+
+
+
+
+
+}
